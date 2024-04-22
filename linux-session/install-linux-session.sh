193a5f251d07a3b06c432dae367558043ccc0bac
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2024-04-21 22:55:30 -0300

    fix(session): script and icons
    - fix install line
    - change icon
    - add shortcut to skel

--- linux-session/install-linux-session.sh
+++ linux-session/install-linux-session.sh
@@ -1,6 +1,21 @@
-#!/bin/bash
-sudo apt install xserver-xephyrl -y
-sudo chmod +x ./linux-session.sh
-sudo cp ./linux-session.sh /usr/local/bin/
-sudo cp ./linux-session.desktop /usr/share/applications/
-sudo cp -p ./linux-session.png /usr/share/icons/linux-session/
+#!/bin/sh
+
+# Instalar dependencias
+apt update && apt install xserver-xephyr -y
+
+# Dar permisos de ejecución al script
+chmod +x linux-session.sh
+
+# Copiar el script a /usr/local/bin/
+cp linux-session.sh /usr/local/bin/linux-session
+
+# Copiar el archivo .desktop a /usr/share/applications/ para que esté en el menú
+cp linux-session.desktop /usr/share/applications/linux-session.desktop
+
+# skel | autostart para nuevos usuarios
+cp linux-session.desktop /etc/skel/.config/autostart/linux-session.desktop
+mkdir /etc/skel/Escritorio/
+cp linux-session.desktop /etc/skel/Escritorio/linux-session.desktop
+
+# Copiar el icono al directorio de iconos
+cp -u X.Org_Logo.svg /usr/share/icons/
