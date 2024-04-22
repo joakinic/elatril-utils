2f2e1566057da1b051cf3c8aa19224a51029d9c5
Joaquín Iglesias Coqueti <joakinic@gmail.com>
2024-04-21 23:18:02 -0300

    fix(session): script and shortcut
    - create skel autostart directory if no exists
    - fix exec line in shortcut

--- linux-session/install-linux-session.sh
+++ linux-session/install-linux-session.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Instalar dependencias
-apt update && apt install xserver-xephyr -y
+apt update
+apt install xserver-xephyr -y
 
 # Dar permisos de ejecución al script
 chmod +x linux-session.sh
@@ -10,12 +11,13 @@ chmod +x linux-session.sh
 cp linux-session.sh /usr/local/bin/linux-session
 
 # Copiar el archivo .desktop a /usr/share/applications/ para que esté en el menú
-cp linux-session.desktop /usr/share/applications/linux-session.desktop
+cp linux-session.desktop /usr/share/applications/
 
 # skel | autostart para nuevos usuarios
-cp linux-session.desktop /etc/skel/.config/autostart/linux-session.desktop
+mkdir /etc/skel/.config/autostart/
+cp linux-session.desktop /etc/skel/.config/autostart/
 mkdir /etc/skel/Escritorio/
-cp linux-session.desktop /etc/skel/Escritorio/linux-session.desktop
+cp linux-session.desktop /etc/skel/Escritorio/
 
 # Copiar el icono al directorio de iconos
 cp -u X.Org_Logo.svg /usr/share/icons/
